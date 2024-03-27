222387c41ff1f9aae516b9fa0b443b2538ed7640
urashin <s.urakawa@hotmail.co.jp>
2024-03-28 08:16:25 +0900

    feat: 試作のためのテストコンポーネントをあらかじめ配置

--- app/pipeline/pipeline_framework.py
+++ app/pipeline/pipeline_framework.py
@@ -9,13 +9,19 @@ from data_extraction_step import DataExtractionStep
 from attribution_processing_step import AttributionProcStep
 from web_scraper_step import WebScraperStep
 from service_catalog_creator_step import ServiceCatalogCreatorStep
+from test_component_a_step import TestComponentAStep
+from test_component_b_step import TestComponentBStep
+from test_component_c_step import TestComponentCStep
 
 # ステップをファクトリーに登録
-StepFactory.register_step('download', DownloadStep)
-StepFactory.register_step('data_extraction', DataExtractionStep)
-StepFactory.register_step('attribution_proc', AttributionProcStep)
+StepFactory.register_step('download_step', DownloadStep)
+StepFactory.register_step('data_extraction_step', DataExtractionStep)
+StepFactory.register_step('attribution_step', AttributionProcStep)
 StepFactory.register_step('web_scraper_step', WebScraperStep)
 StepFactory.register_step('service_catalog_creator_step', ServiceCatalogCreatorStep)
+StepFactory.register_step('test_component_a_step', TestComponentAStep)
+StepFactory.register_step('test_component_b_step', TestComponentBStep)
+StepFactory.register_step('test_component_c_step', TestComponentCStep)
 
 def execute_pipeline(pipeline_config_path):
     with open(pipeline_config_path, 'r') as file:
